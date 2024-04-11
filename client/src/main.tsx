de636bea8010e92b801a9bf92e7e3ca76ab2ba59
Phu Nguyen <tanphu.nguyen@interopera.co>
2024-04-11 17:26:24 +0700

    config database & docker

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -10,28 +10,29 @@ import AddVideoFeature from './features/AddVideo/AddVideoFeature.tsx';
 import { Config } from './config/config.ts';
 
 const socket = new WebSocket(Config.VITE_WS_URL);
-  socket.addEventListener('open', () => {
-    const msg = {
-      command: 'subscribe',
-      identifier: JSON.stringify({
-        channel: 'AlertsChannel'
-      })
-    }
+socket.addEventListener('open', () => {
+  const msg = {
+    command: 'subscribe',
+    identifier: JSON.stringify({
+      channel: 'AlertsChannel'
+    })
+  }
 
-    socket.send(JSON.stringify(msg))
-  });
-  
-  // Listen for messages
-  socket.addEventListener('message', event => {
-    const data = JSON.parse(event.data);
-    if (data?.message?.type === 'alert') {
-      const message = data?.message?.message;
-      const user = localStorage.getItem('user');
-      if (message.indexOf(user) === -1) {
-        alert(message);
-      }
+  socket.send(JSON.stringify(msg))
+});
+
+// Listen for messages
+socket.addEventListener('message', event => {
+  const data = JSON.parse(event.data);
+  if (data?.message?.type === 'alert') {
+    const message = data?.message?.message;
+    const user = localStorage.getItem('user');
+    if (message.indexOf(user) === -1) {
+      alert(message);
     }
-  });
+  }
+});
+
 const router = createBrowserRouter([
   {
     path: "/",
